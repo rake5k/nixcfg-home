9a5b462f8c98bb8895a9cbffd3b8378ca6b56375
Christian Harke <christian@harke.ch>
2023-01-13 15:23:33 +0100

    Update nixcfg input

--- flake.nix
+++ flake.nix
@@ -2,16 +2,13 @@
   description = "NixOS & Home-Manager Configuration - Home Flavor";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixcfg.url = "github:christianharke/nixcfg";
   };
 
   outputs = { self, nixpkgs, nixcfg, ... } @ inputs:
     let
-      nixcfgLib = nixcfg.lib {
-        inherit (inputs.nixcfg) inputs;
-        rootPath = ./.;
-      };
+      nixcfgLib = nixcfg.lib { rootPath = ./.; };
 
       inherit (nixpkgs.lib) listToAttrs recursiveUpdate;
       inherit (nixcfgLib) mkHome mkNixos;

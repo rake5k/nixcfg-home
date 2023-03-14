1f121a3a1caa57da7a9979b6489868ae8faa6e18
Christian Harke <christian@harke.ch>
2023-03-14 21:29:21 +0100

    Update nixcfg input and little refactoring

--- flake.nix
+++ flake.nix
@@ -4,25 +4,25 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixcfg.url = "github:christianharke/nixcfg";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, nixcfg, ... } @ inputs:
     let
       nixcfgLib = nixcfg.lib { inherit inputs; };
 
+      inherit (inputs.flake-utils.lib.system) x86_64-linux;
       inherit (nixpkgs.lib) listToAttrs recursiveUpdate;
-      inherit (nixcfgLib) mkHome mkNixos;
     in
+    with nixcfgLib;
     recursiveUpdate nixcfg {
 
       nixosConfigurations = listToAttrs [
-        (mkNixos "x86_64-linux" "altair")
+        (mkNixos x86_64-linux "altair")
       ];
 
-      checks = {
-        x86_64-linux = {
-          build = self.nixosConfigurations.altair.config.system.build.toplevel;
-        };
-      };
+      checks = mkForEachSystem [
+        (mkBuild "build" self.nixosConfigurations.altair.config.system.build.toplevel)
+      ];
     };
 }

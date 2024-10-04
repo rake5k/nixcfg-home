9bd2f0157fbd367fccb247b5e9eb0834ede22565
Christian Harke <christian@harke.ch>
2024-10-04 23:01:47 +0200

    Configure media keys

--- hosts/malmok/home-christian.nix
+++ hosts/malmok/home-christian.nix
@@ -12,6 +12,12 @@
         xserver = {
           enable = true;
           polybar.monitors.temperature-source = "/sys/devices/platform/coretemp.0/hwmon/hwmon2/temp2_input";
+          xbindkeys.keymap = {
+            #XF86LaunchA = "launch a";
+            #XF86LaunchB = "launch b";
+            XF86KbdBrightnessDown = "brightnessctl set 50%- -d smc::kbd_backlight";
+            XF86KbdBrightnessUp = "brightnessctl set 50%+ -d smc::kbd_backlight";
+          };
           xmonad.lightweight = true;
         };
       };

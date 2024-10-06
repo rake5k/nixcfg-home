c51e9f13d34ff163e4d5740b0d62fc9f212959b6
Christian Harke <christian@harke.ch>
2024-10-06 11:46:35 +0200

    Enable wireguard

--- hosts/malmok/default.nix
+++ hosts/malmok/default.nix
@@ -14,5 +14,30 @@
     };
   };
 
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      address = [
+        "10.0.10.2/32"
+      ];
+      dns = [
+        "10.0.10.1"
+      ];
+      privateKeyFile = "/home/christian/.local/share/secrets/wireguard_key";
+
+      peers = [
+        {
+          publicKey = "TwJwav0wXD9RdE0K2RkBd+Y8yfDkzkw3A4ZoBOOpv2M=";
+          presharedKeyFile = "/home/christian/.local/share/secrets/wireguard_psk";
+          allowedIPs = [
+            "::/0"
+            "0.0.0.0/0"
+          ];
+          endpoint = "ras.harke.ch:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
+
   system.stateVersion = import ./state-version.nix;
 }

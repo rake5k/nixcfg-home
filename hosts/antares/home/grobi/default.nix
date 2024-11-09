a1afa284d53552aece3a918946393443668e9ba8
Christian Harke <christian@harke.ch>
2024-11-09 12:29:25 +0100

    Fix build since wallpapersDir module is not available anymore

--- hosts/antares/home/grobi/default.nix
+++ hosts/antares/home/grobi/default.nix
@@ -1,8 +1,9 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 let
 
-  updateWallpapers = "${lib.getExe pkgs.feh} --no-fehbg --bg-fill --randomize ${config.custom.roles.desktop.wallpapersDir}";
+  ZOWIE-XL-LCD-DP = "BNQ-32555-21573-ZOWIE XL LCD-5AG00657SL0";
+  ZOWIE-XL-LCD-HDMI = "BNQ-32554-21573-ZOWIE XL LCD-5AG00657SL0";
 
 in
 
@@ -13,18 +14,17 @@ in
     rules = [
       {
         name = "DP";
-        outputs_connected = [ "DP-4-BNQ-32555-21573-ZOWIE XL LCD-5AG00657SL0" ];
+        outputs_connected = [ "DP-4-${ZOWIE-XL-LCD-DP}" ];
         configure_single = "DP-1";
         primary = "DP-1";
         atomic = true;
         execute_after = [
           "${pkgs.xorg.xrandr}/bin/xrandr --output DP-4 --mode 1920x1080 --rate 60"
-          updateWallpapers
         ];
       }
       {
         name = "HDMI";
-        outputs_connected = [ "HDMI-0-BNQ-32554-21573-ZOWIE XL LCD-5AG00657SL0" ];
+        outputs_connected = [ "HDMI-0-${ZOWIE-XL-LCD-HDMI}" ];
         configure_single = "HDMI-0";
         primary = "HDMI-0";
         atomic = true;

5944f07e1b36582650d40ac8ceb9d41508c1ab1c
Christian Harke <christian@harke.ch>
2025-01-23 23:52:39 +0100

    Enable nvidia drivers and gpu powermanagement

--- hosts/hyperion/hardware/default.nix
+++ hosts/hyperion/hardware/default.nix
@@ -1,13 +1,23 @@
-{ pkgs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [
     # Include the results of the hardware scan.
     ./configuration.nix
     ./disk-config.nix
+
+    # nixos-hardware
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
   ];
 
   environment.systemPackages = with pkgs; [
     openseachest
   ];
+
+  # See: https://github.com/NixOS/nixos-hardware/issues/348#issuecomment-997123102
+  hardware.nvidia = {
+    open = true;
+    powerManagement.enable = true;
+    nvidiaPersistenced = true;
+  };
 }

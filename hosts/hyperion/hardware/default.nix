92cfc7eaef7e3348b4f51d1865c68fb0262630d3
Christian Harke <christian@harke.ch>
2025-01-27 13:21:50 +0100

    Fix plex hardware transcoding

--- hosts/hyperion/hardware/default.nix
+++ hosts/hyperion/hardware/default.nix
@@ -1,23 +1,38 @@
-{ inputs, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     # Include the results of the hardware scan.
     ./configuration.nix
     ./disk-config.nix
-
-    # nixos-hardware
-    inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
   ];
+
+  boot = {
+    # See: https://www.reddit.com/r/jellyfin/comments/ldzhhl/gpu_transcoding_issues/
+    kernelModules = [
+      "nvidia"
+      "nvidia_modeset"
+      "nvidia_drm"
+      "nvidia_uvm"
+    ];
+
+    blacklistedKernelModules = [ "nouveau" ];
+  };
 
   environment.systemPackages = with pkgs; [
     openseachest
   ];
 
   # See: https://github.com/NixOS/nixos-hardware/issues/348#issuecomment-997123102
-  hardware.nvidia = {
-    open = true;
-    powerManagement.enable = true;
-    nvidiaPersistenced = true;
+  hardware = {
+    graphics.enable = true;
+    nvidia = {
+      open = true;
+      powerManagement.enable = true;
+      nvidiaPersistenced = true;
+    };
   };
+
+  # See: https://discourse.nixos.org/t/jellyfin-hardware-encoding-error-cannot-load-libcuda-so-1/29427/5
+  services.xserver.videoDrivers = [ "nvidia" ];
 }

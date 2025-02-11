1a6e9a7424df707d3548a5d0895a4ef598dc4ae3
Christian Harke <christian@harke.ch>
2025-02-11 22:45:04 +0100

    Drop checks which are built by Garnix anyways

--- flake.nix
+++ flake.nix
@@ -48,14 +48,6 @@
 
       checks = recursiveUpdate nixcfg.checks (
         mkForSystem aarch64-linux [ (mkBuild "build" self.nixOnDroidConfigurations.io.activationPackage) ]
-        // (mkForSystem x86_64-linux [
-          (mkBuild "build" self.nixosConfigurations.altair.config.system.build.toplevel)
-          (mkBuild "build" self.nixosConfigurations.antares.config.system.build.toplevel)
-          (mkBuild "build" self.nixosConfigurations.hyperion.config.system.build.toplevel)
-          (mkBuild "build" self.nixosConfigurations.malmok.config.system.build.toplevel)
-          (mkBuild "build" self.nixosConfigurations.sirius-b.config.system.build.toplevel)
-          (mkBuild "build-deck@sirius-a" self.homeConfigurations."deck@sirius-a".activationPackage)
-        ])
       );
 
       devShells = mkForEachSystem [ (mkDevShell "default" { flake = self; }) ];
